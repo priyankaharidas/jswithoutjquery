e18ebac3103d9712c473abcacb9a8dd1df0dfe6e
priyanka <priyanka.usapp@gmail.com>
2017-04-11 14:49:05 -0400

    adding functionality to the button

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-
+var unlis=document.getElementById("list");
+var ourbutton=document.getElementById("blue");
 var ourheading=document.getElementById("heading");
 var ourlist=document.getElementById("list").getElementsByTagName("li");
 for(i=0; i<ourlist.length; i++)
 {
 	ourlist[i].addEventListener("click",activateItem);
-	
+}	
 	function activateItem()
 	{
 		ourheading.innerHTML=this.innerHTML;
 	}
-}
+	
+	ourbutton.addEventListener("click",createNewItem);
+	
+	function createNewItem()
+	{
+		unlis.innerHTML +="<li>something new</li>";
+	}
